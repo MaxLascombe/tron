e5bcb1ecf8b7a190b4a78b7c84559652d20daf84
Max Lascombe <mtl2154@columbia.edu>
2023-03-14 14:20:08 -0400

    Added start menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import GameCanvas from './GameCanvas'
+import Menu from './components/Menu'
 import MusicPopup from './components/MusicPopup'
 import PausePopup from './components/PausePopup'
 import { useKeyAction } from './hooks/useKeyAction'
@@ -26,6 +27,7 @@ const App = () => {
 
   return (
     <div className='h-screen w-full bg-black p-4 font-mono'>
+      <Menu show={gameStatus === 'menu'} />
       <PausePopup paused={gameStatus === 'paused'} />
       <GameCanvas key={gameKey} {...{ gameStatus, setGameStatus }} />
     </div>
---
+++ src/components/Menu.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import Popup from './Popup'
+import { useAnimationTimeout } from '../hooks/useAnimationFrame'
+
+const Menu = ({ show }: { show: boolean }) => {
+  const [clicked, setClicked] = useState(false)
+  useAnimationTimeout(() => setClicked(c => !c), 500)
+
+  return (
+    <Popup show={show} blur={true}>
+      Welcome to
+      <h1 className='p-4 text-3xl'>TRON</h1>
+      Press Space to begin.
+      <div className='mt-2'>
+        <div
+          className={
+            'inline-block rounded-xl border-2 border-white px-8 py-2 text-xs ' +
+            (clicked ? 'mb-0.5 mt-1 border-b-2' : 'border-b-8')
+          }>
+          SPACE
+        </div>
+      </div>
+    </Popup>
+  )
+}
+
+export default Menu
